2ce1b79ce6aa1976130919cbe14c24a0604f977e
pro <faxridinkf@gmail.com>
2022-01-14 10:11:38 +0500

    data,joinPage и тд

---
+++ src/components/JoinPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function JoinPage() {
+  return (
+    <div className='join-block'>
+      <input placeholder='ID Room' type='text' />
+      <input placeholder='Your Name or Nickname' type='text' />
+      <button className='btn btn-success'>Sign in</button>
+    </div>
+  )
+}
+export default JoinPage
